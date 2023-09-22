f193b58e4ba4d6a3d2958fd0fdf19ac949181f89
Jorge Cimentada <cimentadaj@gmail.com>
2023-09-22 12:11:49 +0200

    Updates new shiny template

--- un_shiny.R
+++ un_shiny.R
@@ -4,117 +4,114 @@ library(shiny.router)
 library(shinyjs)
 library(shinycssloaders)
 
+# Constants
 background_color <- "#F4F4F4"
-video_id <- "-EVgyaKO6vU" # Replace with the ID of your YouTube video
+video_id <- "-EVgyaKO6vU"
 logo_url <- "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/UN_emblem_blue.svg/1205px-UN_emblem_blue.svg.png"
 
-# Menu
-menu <- div(
-  style = paste("display: flex; justify-content: center; padding: 10px 0;"),
+# Create Main Menu UI Component
+menu_ui <- function() {
   div(
-    class = "ui secondary menu",
-    tags$img(src = logo_url, style = "height: 50px; margin-right: 20px;"),
-    a(class = "item", style = "color: black;", href = route_link("index"), icon("home"), "Home"),
-    a(class = "item", style = "color: black;", href = route_link("other"), icon("clock"), "Other")
-  )
-)
-
-page1_ui <- function() {
-  div(
-    class = "ui big button",
-    id = "start_button",
-    "Start"
-  )
-}
-
-page2_ui <- function() {
-  div(
-    fileInput("csv_file", "Upload Data", accept = ".csv"),
-    withSpinner(uiOutput("more"))
-  )
-}
-
-page3_ui <- function() {
-  div(
-    menu,  # Integrated menu
-  )
-}
-
-page <- function(title, content) {
-  div(
-    style = paste("background-color:", background_color, "; height: 100vh; display: flex; flex-direction: column;"), # Flexbox styles
-    menu, # Menu at the top
+    style = "display: flex; justify-content: center; padding: 10px 0;",
     div(
-      style = "flex: 1; display: flex; justify-content: center; align-items: center;", # Content wrapper
+      class = "ui secondary menu",
+      tags$img(src = logo_url, style = "height: 50px; margin-right: 20px;"),
+      a(class = "item", style = "color: black;", href = route_link("index"), icon("home"), "Home"),
+      a(class = "item", style = "color: black;", href = route_link("data_loader"), "Data Loader"),
+      a(class = "item", style = "color: black;", href = route_link("analysis"), "Analysis")
+    )
+  )
+}
+
+# Create Page UI Wrapper
+page_ui <- function(content) {
+  div(
+    style = paste("background-color:", background_color, "; height: 100vh; display: flex; flex-direction: column;"),
+    menu_ui(),
+    div(
+      style = "flex: 1; display: flex; justify-content: center; align-items: center;",
       div(
         class = "ui container",
         div(
           class = "ui grid",
-          div(
-            class = "twelve wide column centered", # Centered content column
-            div(
-              class = "ui segment",
-              p(content)
-            )
-          )
+          div(class = "twelve wide column centered", div(class = "ui segment", content))
         )
       )
     )
   )
 }
 
-root_page <- page(
-  "Home page",
-  div(
-    style = "display: flex; justify-content: center; align-items: center; height: 100%;",
-    div(
-      p("Welcome on sample routing page!"), # This is the text
-      tags$iframe(
-        src = paste0("https://www.youtube.com/embed/", video_id),
-        width = "560",
-        height = "315",
-        frameborder = "0",
-        allowfullscreen = TRUE,
-        style = "display: block; margin: 0 auto;"
-      )
+# Create Home Page UI
+home_page_ui <- function() {
+  page_ui(
+    tags$iframe(
+      src = paste0("https://www.youtube.com/embed/", video_id),
+      width = "560",
+      height = "315",
+      frameborder = "0",
+      allowfullscreen = TRUE,
+      style = "display: block; margin: 0 auto;"
     )
   )
-)
+}
 
-## root_page <- page("Home page", "Welcome on sample routing page!")
-other_page <- page("Some other page", "Lorem ipsum dolor sit amet.")
+# Create Data Loader Page UI
+data_loader_page_ui <- function() {
+  page_ui(
+    div(fileInput("csv_file", "Upload Data", accept = ".csv"), withSpinner(uiOutput("more")))
+  )
+}
 
+# Create Analysis Page UI
+analysis_page_ui <- function() {
+  page_ui(
+    div(
+      uiOutput("alert_content"),
+      div(id = "analysis_content")
+    )
+  )
+}
 
+# Main UI
 ui <- semanticPage(
-  title = "Three-Step App",
+  title = "UN-Themed Shiny App",
   router_ui(
-    route("page1", page1_ui()),
-    route("page2", page2_ui()),
-    route("page3", page3_ui()),
-    route("index", root_page),
-    route("other", other_page)
+    route("index", home_page_ui()),
+    route("data_loader", data_loader_page_ui()),
+    route("analysis", analysis_page_ui())
   )
 )
 
+# Server logic
 server <- function(input, output, session) {
-  router_server("page1")
+  router_server("index")
 
-  observeEvent(input$start_button, {
-    change_page("page2")
-  })
+  data_processed <- reactiveVal(FALSE)
 
   output$more <- renderUI({
     req(input$csv_file)
-    inFile <- input$csv_file
-    data <- read.csv(inFile$datapath)
-    Sys.sleep(4)
+    data <- read.csv(input$csv_file$datapath)
+    Sys.sleep(4)  # Emulating data processing delay
+    data_processed(TRUE)
     actionButton("process", "Analysis button")
   })
 
+  output$alert_content <- renderUI({
+    if (!data_processed()) {
+      message_box(class = "warning", header = "Warning", content = "Please upload and process the data first.")
+    }
+  })
+
   observeEvent(input$process, {
-    change_page("page3")
+    change_page("analysis")
+  })
+
+  observe({
+    current_page <- get_page()
+    if (current_page == "analysis" && !data_processed()) {
+      shinyjs::click("alert_content")  # Activate the message_box
+    }
   })
 }
 
 shinyApp(ui, server)
-
