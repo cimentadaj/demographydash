6ce8a9a7f949eb7f68581c8b2151d9e8292dd5eb
Jorge Cimentada <cimentadaj@gmail.com>
2023-10-15 15:05:21 +0200

    Makes intermediatte part of app

--- R/app_server.R
+++ R/app_server.R
@@ -1,41 +1,111 @@
+#' Generate the plot modules after simulation
+#'
+#' @param sim_res List with datasets for simulation.
+#' @importFrom untheme plotWithDownloadButtons
+#' @noRd
+call_modules <- function(sim_res) {
+  callModule(
+    plotWithDownloadButtons,
+    "plot1",
+    data = sim_res()$mtcars,
+    ggplot_obj = create_scatter_plot(sim_res()$mtcars, "mpg", "wt")
+  )
+
+  callModule(
+    plotWithDownloadButtons,
+    "plot2",
+    data = sim_res()$iris,
+    ggplot_obj = create_scatter_plot(sim_res()$iris, "Sepal.Length", "Sepal.Width")
+  )
+
+  callModule(
+    plotWithDownloadButtons, "plot3",
+    data = sim_res()$mtcars,
+    ggplot_obj = create_scatter_plot(sim_res()$mtcars, "mpg", "wt"),
+    update_ggplot_func = update_ggplot_func
+  )
+}
+
+
+#' Create a basic scatter plot
+#'
+#' @param data The data frame to be used for plotting.
+#' @param x_var The name of the column to be used for the x-axis.
+#' @param y_var The name of the column to be used for the y-axis.
+#' @import ggplot2
+#' @return A ggplot2 object
+#' @noRd
+create_scatter_plot <- function(data, x_var, y_var) {
+  ggplot(data, aes_string(x = x_var, y = y_var)) +
+    geom_point()
+}
+
+#' Update a ggplot object's y-axis to percentage scale
+#'
+#' @param ggplot_obj The ggplot2 object to be updated.
+#' @param scale_type The type of scale to be applied.
+#' @importFrom scales percent
+#' @return An updated ggplot2 object
+#' @noRd
+update_ggplot_func <- function(ggplot_obj, scale_type) {
+  if (scale_type == "Percentage") {
+    ggplot_obj + scale_y_continuous(labels = percent)
+  } else {
+    ggplot_obj
+  }
+}
+
 #' The application server-side logic
 #'
 #' This function defines the server logic for the Shiny application, managing data processing, UI rendering, and routing.
 #'
 #' @param input,output,session Internal parameters for `{shiny}`.
-#' @importFrom shiny reactiveVal renderUI req actionButton observeEvent observe
-#' @importFrom shiny.router router_server change_page get_page
-#' @importFrom shiny.semantic message_box
-#' @importFrom shinyjs click
-#' @importFrom utils read.csv
+#' @importFrom shinyjs hide show
+#' @importFrom shiny renderPlot observeEvent callModule renderTable reactive reactiveVal updateNumericInput
+#' @importFrom untheme plotWithDownloadButtons
 #' @noRd
 app_server <- function(input, output, session) {
-  router_server("index")
-
-  data_processed <- reactiveVal(FALSE)
-
-  output$more <- renderUI({
-    req(input$csv_file)
-    data <- read.csv(input$csv_file$datapath)
-    Sys.sleep(4) # Emulating data processing delay
-    data_processed(TRUE)
-    actionButton("process", "Analysis button")
+  reactive_mtcars <- reactive({
+    datasets::mtcars[datasets::mtcars$cyl == input$cylinders, ]
   })
 
-  output$alert_content <- renderUI({
-    if (!data_processed()) {
-      message_box(class = "warning", header = "Warning", content = "Please upload and process the data first.")
-    }
+  output$plot0 <- renderPlot({
+    data1 <- reactive_mtcars()
+    create_scatter_plot(data1, "mpg", "wt")
   })
 
-  observeEvent(input$process, {
-    change_page("analysis")
+  output$myTable <- renderTable(reactive_mtcars()[1:5, 1:3])
+
+  observeEvent(input$forward, {
+    hide("step1")
+    show("step2")
+    updateNumericInput(session, "step", value = 2)
   })
 
-  observe({
-    current_page <- get_page()
-    if (current_page == "analysis" && !data_processed()) {
-      click("alert_content") # Activate the message_box
-    }
+  observeEvent(input$back_to_step1, {
+    hide("step2")
+    show("step1")
+    updateNumericInput(session, "step", value = 1)
+  })
+
+  # Define a reactiveVal to store simulation results
+  simulation_results <- reactiveVal()
+
+  observeEvent(input$begin, {
+
+    simulation_results(run_simulation(reactive_mtcars())) # Update simulation_results
+
+    hide("step2")
+    show("step3")
+
+    call_modules(simulation_results)
+    updateNumericInput(session, "step", value = 3)
+  })
+
+  observeEvent(input$back_to_step2, {
+    hide("step3")
+    show("step2")
+    updateNumericInput(session, "step", value = 2)
   })
 }
+
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,129 +1,122 @@
-## https://github.com/Appsilon/shiny.semantic/blob/develop/examples/grid/app.R
-
-background_color <- "#FFFFFF"
-video_id <- "-EVgyaKO6vU"
-logo_url <- "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/UN_emblem_blue.svg/1205px-UN_emblem_blue.svg.png"
-## logo_url <- here::here("inst/app/www/un_logo.png")
-
-
-#' The application User-Interface
+#' Define a simple grid layout for the application
 #'
-#' @param request Internal parameter for `{shiny}`.
-#' @return A shiny semantic UI for the application.
-#' @importFrom shiny.semantic semanticPage
-#' @importFrom shiny.router router_ui route
+#' @return A grid template for shiny.semantic layout
+#' @importFrom shiny.semantic grid_template
 #' @noRd
-app_ui <- function(request) {
-  ui <- semanticPage(
-    title = "UN-Themed Shiny App",
-    router_ui(
-      route("index", home_page_ui()),
-      route("data_loader", data_loader_page_ui()),
-      route("analysis", analysis_page_ui())
-    )
-  )
-
-  ui
-}
-
-#' Create Main Menu UI Component
-#'
-#' @return A shiny UI div containing the main menu.
-#' @importFrom shiny div tags a icon
-#' @importFrom shiny.router route_link
-menu_ui <- function() {
-  shiny::div(
-    style = "width: 75%; margin: auto;",  # Constrain and center the entire content
-    shiny::div(
-      style = "display: flex; flex-direction: column; align-items: center; padding: 10px 0; width: 100%;",
-      shiny::div(
-        style = "text-align: left; width: 100%; border-bottom: 1px solid lightgrey;",
-        shiny::tags$img(src = logo_url, style = "height: 50px; margin-right: 20px;")
+mySimpleGrid <- function() {
+  grid_template(
+    default = list(
+      areas = rbind(
+        c("center", "right")
       ),
-      shiny::div(
-        class = "ui secondary menu",
-        style = "width: 100%; justify-content: center; background-color: #333333;",
-        shiny::tags$style(HTML("
-          .item::after {
-            content: '';
-            position: absolute;
-            right: 0;
-            top: 0;
-            height: 100%;
-            border-right: 1px solid white;
-          }
-          .item:last-child::after {
-            border-right: none;
-          }
-        ")),
-        shiny::a(class = "item", style = "color: white; position: relative;", href = shiny.router::route_link("index"), "Home"),
-        shiny::a(class = "item", style = "color: white; position: relative;", href = shiny.router::route_link("data_loader"), "Data Loader"),
-        shiny::a(class = "item", style = "color: white; position: relative;", href = shiny.router::route_link("analysis"), "Analysis")
-      )
+      rows_height = c("auto"),
+      cols_width = c("2fr", "1fr")
     )
   )
 }
 
-#' Create Page UI Wrapper
+#' Generate a tabset for the application
 #'
-#' @param content Content to be wrapped in the page UI.
-#' @return A shiny UI div for the page layout.
-#' @importFrom shiny div
-page_ui <- function(content) {
-  shiny::div(
-    style = paste("background-color:", background_color, "; height: 100vh; display: flex; flex-direction: column;"),
-    menu_ui(),
-    shiny::div(
-      style = "flex: 1; display: flex; justify-content: center; align-items: center;",
-      shiny::div(
-        class = "ui container",
-        shiny::div(
-          class = "ui grid",
-          shiny::div(class = "twelve wide column centered", content) # Removed 'ui segment' class here
+#' @return A tabset UI component for the application
+#' @importFrom shiny.semantic tabset
+#' @importFrom untheme plotWithDownloadButtonsUI
+#' @importFrom shiny actionButton div
+#' @noRd
+app_tabset <- function() {
+  div(
+    tabset(
+      tabs = list(
+        list(
+          menu = "Tab 1",
+          content = plotWithDownloadButtonsUI("plot1"),
+          id = "first_tab"
+        ),
+        list(
+          menu = "Tab 2",
+          content = plotWithDownloadButtonsUI("plot2"),
+          id = "second_tab"
+        ),
+        list(
+          menu = "Tab 3",
+          content = plotWithDownloadButtonsUI("plot3", radio_choices = c("Absolute", "Percentage")),
+          id = "third_tab"
         )
       )
     )
   )
 }
 
-#' Create Home Page UI
+# Define the run_simulation function
+run_simulation <- function(filtered_data) {
+  message("Running simulation")
+  Sys.sleep(10)
+  # For simplicity, let's assume the simulation returns the same dataset
+  # In reality, you'd have your simulation logic here
+  list(mtcars = filtered_data, iris = datasets::iris)
+}
+
+#' The application User-Interface
 #'
-#' @return A shiny UI for the home page.
-#' @importFrom shiny tags
-home_page_ui <- function() {
-  page_ui(
-    tags$iframe(
-      src = paste0("https://www.youtube.com/embed/", video_id),
-      width = "560",
-      height = "315",
-      frameborder = "0",
-      allowfullscreen = TRUE,
-      style = "display: block; margin: 0 auto;"
+#' @param request Internal parameter for `{shiny}`.
+#' @return A shiny semantic UI for the application.
+#' @importFrom shiny div selectInput actionButton numericInput
+#' @importFrom shinyjs useShinyjs hidden
+#' @importFrom shiny.semantic main_panel
+#' @importFrom untheme fluidUnTheme
+#' @noRd
+# The application User-Interface
+app_ui <- function(request) {
+  fluidUnTheme(
+    useShinyjs(),
+    main_panel(
+      div(
+        id = "step1",
+        selectInput("cylinders", "Select Number of Cylinders:",
+          choices = unique(datasets::mtcars$cyl),
+          selected = 6
+        ),
+        actionButton("forward", "Forward")
+      ),
+      hidden(
+        div(
+          id = "step2",
+          actionButton("back_to_step1", "Back"),
+          step_one_ui(),
+          actionButton("begin", "Begin"),
+        )
+      ),
+      hidden(
+        div(
+          id = "step3",
+          actionButton("back_to_step2", "Back"),
+          app_tabset()
+        )
+      ),
+      hidden(
+        numericInput("step", label = NULL, value = 1)
+      )
     )
   )
 }
 
-#' Create Data Loader Page UI
+#' UI component for step one
 #'
-#' @return A shiny UI for the data loader page.
-#' @importFrom shiny div uiOutput
-#' @importFrom shiny.semantic fileInput
-#' @importFrom shinycssloaders withSpinner
-data_loader_page_ui <- function() {
-  page_ui(
-    div(fileInput("csv_file", "Upload Data", accept = ".csv"), withSpinner(uiOutput("more")))
-  )
-}
-
-#' Create Analysis Page UI
-#'
-#' @return A shiny UI for the analysis page.
-#' @importFrom shiny div uiOutput
-analysis_page_ui <- function() {
-  page_ui(
-    div(
-      uiOutput("alert_content"),
-      div(id = "analysis_content")
+#' @return A div containing UI elements for step one
+#' @importFrom shiny actionButton plotOutput tableOutput
+#' @importFrom shiny.semantic grid
+#' @noRd
+step_one_ui <- function() {
+  div(
+    id = "step1",
+    grid(
+      mySimpleGrid(),
+      container_style = "border: 3px solid #4D4D4D",
+      area_styles = list(
+        center = "background: #4D4D4D",
+        right = "border-left: 3px solid #4D4D4D"
+      ),
+      center = plotOutput("plot0"), # Replace with your plot
+      right = tableOutput("myTable") # Replace with your table
     )
   )
 }
